8e09f8c583a6a99d08a4425200b6f565e5f00a9a
drusjs <magnojoky962@gmail.com>
2025-01-20 18:22:49 +0300

    fix

--- js/view3D.js
+++ js/view3D.js
@@ -23,41 +23,52 @@ import
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'; 
 
-export const View3D = ( domContainer ) =>
+export const View3D = ( domContainer, url ) =>
 {		
 	let width = 0;
 	let height = 0;
 	let renderer, camera, scene, light, controls, container;	
 	let needsRender = true;
-	let lastProjectionPoints = null;
 
 	const instance = new EventDispatcher();
 	const loader = new GLTFLoader();
-	const points = [];		
+	const points = [];	
+	const canvas = document.createElement( 'canvas' );
+	const context = canvas.getContext( '2d' );
+
+	const stringifyVector3 = ( v ) => v.x + ',' + v.y + ',' + v.z;
+	const parseVector3 = ( s ) => 
+	{
+		const [ x, y, z ] = s.split( ',' ).map( v => Number.parseFloat( v ) );
+		return Number.isNaN( x ) || Number.isNaN( y ) || Number.isNaN( z ) ? null : new Vector3( x, y, z );
+	};
+	
+	const stringifySpherical = ( s ) => s.radius + ',' + s.phi + ',' + s.theta;
+	const parseSpherical = ( s ) => 
+	{
+		const [ radius, phi, theta ] = s.split( ',' ).map( v => Number.parseFloat( v ) );
+		return Number.isNaN( radius ) || Number.isNaN( phi ) || Number.isNaN( theta ) ? null : new Spherical( radius, phi, theta );
+	};	
 	
 	const fromJSON = ( json ) =>
-	{
+	{	
 		const _points = [];
-		const parseVector3 = ( json ) => new Vector3( json.x, json.y, json.z );
-		const parseSpherical = ( json ) => new Spherical( json.radius, json.phi, json.theta );
 		
 		try
 		{
 			json.forEach( point => 
-			{						
-				_points.push
-				( {
-					// text:				point.text,
-					target:				container.getObjectByName( point.target ),
-					localNormalPoint: 	parseVector3( point.localNormalPoint ),
-					localPoint: 		parseVector3( point.localPoint ),
-					spherical:			parseSpherical( point.spherical ), 
-					offsetCenter:		point.offsetCenter,
-					radius:				point.radius,
-					uuid:				point.uuid,	
-					added:				true,
-					removed:			false,
-				} );
+			{	
+				const target = container.getObjectByName( point.name );
+				
+				if( target )
+				{
+					const uuid = point.point_id;
+					const position = parseVector3( point.position );
+					const spherical = parseSpherical( point.spherical );
+					
+					if( uuid && position && spherical )
+						_points.push( { uuid, target, position, spherical, added:true, removed:false } );
+				}
 			} );
 		}
 		catch( error )
@@ -89,13 +100,12 @@ export const View3D = ( domContainer ) =>
 		scene = new Scene();
 		scene.add( camera = new PerspectiveCamera( 45, 1.0, 10, 10000 ) );
 		scene.add( container = new Group() );
-		// scene.add( new GridHelper( 100, 100, 0xEEEEEE, 0xEEEEEE ) );
 		scene.add( new AmbientLight( 0xFFFFFF, 1 ) );
 		scene.add( light );
 
 
 		controls = new OrbitControls( camera, renderer.domElement );
-		controls.addEventListener( 'change', onControlsChange );
+		controls.addEventListener( 'change', update );
 		controls.screenSpacePanning = true;
 		controls.enableDamping = true;
 		controls.dampingFactor = 0.1;
@@ -112,7 +122,7 @@ export const View3D = ( domContainer ) =>
 		controls.minAzimuthAngle = -Infinity;
 		controls.maxAzimuthAngle = Infinity;
 
-		loader.load( 'AmoebaBabylonNoPlates.glb', ( gltf ) => 
+		loader.load( url, ( gltf ) => 
 		{	
 			container.add( gltf.scene );
 
@@ -124,24 +134,21 @@ export const View3D = ( domContainer ) =>
 		} );
 	};
 	
-	const onControlsChange = () => 
-	{
-		needsRender = true;
-	};
+	const update = () => needsRender = true;
 
 	const fitToScreen = () =>
 	{
 		container.updateMatrixWorld( true );
-			
+
 		const box = new Box3().expandByObject( container );
-		const maxSize = box.getBoundingSphere( new Sphere() ).radius * 2;					
+		const maxSize = box.getBoundingSphere( new Sphere() ).radius * 2;
 		const fitHeightDistance = maxSize / ( 2 * Math.atan( Math.PI * camera.fov / 360 ) );
 		const fitWidthDistance = fitHeightDistance / camera.aspect;
 		const distance = Math.max( fitHeightDistance, fitWidthDistance );
 		const direction = controls.target.clone().sub( camera.position ).normalize().multiplyScalar( distance );
 		const center = box.getCenter( new Vector3() );
 
-		controls.minDistance = controls.maxDistance = distance * 0.98; 
+		controls.minDistance = controls.maxDistance = distance * 1;//0.98;
 		controls.target.copy( center );
 
 		camera.near = distance / 100;
@@ -153,135 +160,17 @@ export const View3D = ( domContainer ) =>
 		controls.update();
 		controls.minDistance /= 256;
 		controls.maxDistance *= 2;
+		
+		needsRender = true;
 	};
 	
-	const render = ( timeStamp = null ) =>
-	{
-		const rect = domContainer.getBoundingClientRect();
-		
-		if( rect.width != width || rect.height != height )
-		{
-			width = rect.width;
-			height = rect.height;
-			
-			camera.aspect = width / height;
-			camera.updateProjectionMatrix();
-
-			renderer.setSize( width, height );
-
-			needsRender = true;
-		}
-
-		controls.update();
-		
-		const projectionUUIDS = [];
-		const projectionPoints = [];
-
-		if( needsRender )
-		{
-			camera.updateMatrixWorld();
-
-			for( let i = points.length - 1; i >= 0; i-- )
-			{	
-				const point = points[ i ];
-				const { /*text,*/ target, localNormalPoint, radius, localPoint, uuid, added, removed, offsetCenter, spherical } = point;
-				const targetBox3 = new Box3().expandByObject( target );
-				const targetCenter = targetBox3.getCenter( new Vector3() );
-				const worldPoint = target.localToWorld( localPoint.clone() );
-				const direction = ( () => 
-				{
-					if( offsetCenter == 'world' )
-						return worldPoint.clone().normalize();
-					else if( offsetCenter == 'local' )
-						return worldPoint.clone().sub( targetCenter ).normalize();
-					else if( offsetCenter == 'spherical' )
-						return new Vector3().setFromSpherical( spherical ).normalize();
-					else if( offsetCenter == 'normal' )
-						return target.localToWorld( localNormalPoint.clone() ).sub( worldPoint ).normalize();
-					else 
-						throw new Error( 'WTF?' );
-				} )();
-
-				const projection = worldPoint.clone().project( camera );
-				const offsetPoint = worldPoint.clone().addScaledVector( direction, radius );
-				const offsetProjection = offsetPoint.clone().project( camera );
-
-				if( !removed )
-				{
-					const surfacePoint = 
-					{
-						type:'surface',
-						uuid,
-						//text,
-						x:rect.x + ( projection.x * rect.width / 2 ) + rect.width / 2,
-						y:rect.y - ( projection.y * rect.height / 2 ) + rect.height / 2,
-						z:projection.z,
-						visible:projection.z <= 1.0,
-					};
-					
-					const offsetPoint = 
-					{
-						type:'offset',
-						uuid,
-						//text,
-						x:rect.x + ( offsetProjection.x * rect.width / 2 ) + rect.width / 2,
-						y:rect.y - ( offsetProjection.y * rect.height / 2 ) + rect.height / 2,
-						z:offsetProjection.z,
-						visible:offsetProjection.z <= 1.0,
-					};
-					
-					projectionUUIDS.push( uuid );	
-					projectionPoints.push( surfacePoint, offsetPoint );
-					
-					if( added )
-					{
-						point.added = false;
-						
-						instance.dispatchEvent( { type:'added', uuid, points:[ surfacePoint, offsetPoint ] } );
-					}
-				}
-				else 
-				{
-					points.splice( i, 1 );
-
-					instance.dispatchEvent( { type:'removed', uuid, points:
-					[
-						{ type:'surface', uuid },
-						{ type:'offset', uuid },
-					] } );
-				}
-			};
-
-			projectionPoints.sort( ( a, b ) => b.z - a.z );
-			
-			lastProjectionPoints = projectionPoints;
-
-			renderer.render( scene, camera );
-		}
-		
-		window.requestAnimationFrame( render );
-		
-		if( needsRender )
-			instance.dispatchEvent( { type:'update', uuids:projectionUUIDS, points:projectionPoints } );
-			
-		needsRender = false;
-	}
-	
-	init();
-	
-	const canvas = document.createElement( 'canvas' );
-	const context = canvas.getContext( '2d' );
-		
-	instance.fromJSON = fromJSON;
-	instance.reset = () =>
+	const reset = () =>
 	{
 		controls.reset();
-		
 		fitToScreen();
 	};
-	instance.fitToScreen = fitToScreen;
-	instance.update = () => needsRender = true;
-	instance.getCanvas = () => 
+
+	const getCanvas = () => 
 	{
 		canvas.width = width;
 		canvas.height = height;
@@ -290,6 +179,108 @@ export const View3D = ( domContainer ) =>
 		return canvas;
 	};
 	
+	const render = ( timeStamp = null ) =>
+	{
+		const rect = domContainer.getBoundingClientRect();
+
+		if( rect.width != width || rect.height != height )
+		{				
+			width = rect.width;
+			height = rect.height;
+
+			camera.aspect = width / height;
+			camera.updateProjectionMatrix();
+
+			renderer.setSize( width, height );
+			
+			needsRender = true;
+		}
+
+		controls.update();
+		
+		if( needsRender )
+		{
+			needsRender = false;
+			
+			const projectionUUIDS = [];
+			const projectionPoints = [];
+
+			camera.updateMatrixWorld();
+
+			for( let i = points.length - 1; i >= 0; i-- )
+			{
+				const point = points[ i ];
+				const { target, position, uuid, time, added, removed, spherical } = point;
+				const worldPoint = target.localToWorld( position.clone() );
+				const direction = new Vector3().setFromSpherical( spherical ).normalize();
+
+				//console.log( direction );
+
+				const projection = worldPoint.clone().project( camera );
+				const offsetPoint = worldPoint.clone().addScaledVector( direction, spherical.radius );
+				const offsetProjection = offsetPoint.clone().project( camera );
+
+				if( !removed )
+				{
+					const surfacePoint =
+					{
+						type:'surface',
+						uuid,
+						x: ( projection.x * rect.width / 2 ) + rect.width / 2, // rect.x + 
+						y:-( projection.y * rect.height / 2 ) + rect.height / 2, // rect.x + 
+						z:projection.z,
+						visible:projection.z <= 1.0,
+					};
+
+					const offsetPoint =
+					{
+						type:'offset',
+						uuid,
+						x: ( offsetProjection.x * rect.width / 2 ) + rect.width / 2, // rect.x + 
+						y:-( offsetProjection.y * rect.height / 2 ) + rect.height / 2, // rect.x + 
+						z:offsetProjection.z,
+						visible:offsetProjection.z <= 1.0,
+					};
+
+					projectionUUIDS.push( uuid );
+					projectionPoints.push( surfacePoint, offsetPoint );
+
+					if( added )
+					{
+						point.added = false;
+
+						instance.dispatchEvent( { type:'added', time, uuid, points:[ surfacePoint, offsetPoint ] } );
+					}
+				}
+				else
+				{
+					points.splice( i, 1 );
+
+					instance.dispatchEvent( { type:'removed', uuid, time:performance.now(), points:
+					[
+						{ type:'surface', uuid },
+						{ type:'offset', uuid },
+					] } );
+				}
+			};
+
+			projectionPoints.sort( ( a, b ) => b.z - a.z );
+
+			//
+
+			renderer.render( scene, camera );
+		
+
+			instance.dispatchEvent( { type:'update', uuids:projectionUUIDS, points:projectionPoints } );
+		}
+		
+		window.requestAnimationFrame( render );
+	}
+
+	init();
+
+
+	Object.assign( instance, { fromJSON, reset, fitToScreen, update, getCanvas } );
 
 	return instance;
 };
